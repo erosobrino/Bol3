9406fb5367ba4371624ea6168e5a42d05d5faa88
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2018-10-08 17:31:37 +0200

    Segundo Commit

---
+++ Ejer4/Ejer4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer4
+{
+    class Ejer4
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        public double calculo(char simbolo, String numeros)
+        {
+            double resultado = -1;
+            switch (simbolo)
+            {
+                case '+':
+                    break;
+                case '-':
+                    break;
+                case '*':
+                    break;
+                case '/':
+                    break;
+                default:
+                    break;
+            }
+
+            return resultado;
+        }
+    }
+}
