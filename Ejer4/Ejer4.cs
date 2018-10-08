d56fe969b1a63c26a90c0ac9087579feac60e9cc
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2018-10-08 17:43:42 +0200

    Tercer Commit

--- Ejer4/Ejer4.cs
+++ Ejer4/Ejer4.cs
@@ -10,6 +10,9 @@ namespace Ejer4
     {
         static void Main(string[] args)
         {
+            Ejer4 ejer = new Ejer4();
+            Console.WriteLine((int)ejer.calculo('+', "2346"));
+            Console.ReadKey();
         }
 
 
@@ -19,6 +22,14 @@ namespace Ejer4
             switch (simbolo)
             {
                 case '+':
+                    resultado = 0;
+                    for (int i = 0; i < numeros.Length; i++)
+                    {
+                        Console.WriteLine(numeros[i]);
+                        resultado =resultado+(double)numeros[i];
+                        Console.WriteLine("result: " +resultado);
+                        
+                    }
                     break;
                 case '-':
                     break;
